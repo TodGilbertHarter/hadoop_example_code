892719631e3b71087a43c3072a5c0683a11dd842
demo <demo@sandbox.hortonworks.com>
2017-06-27 17:45:52 +0000

    updated latha scripts

--- mapreduce-test/src/main/resources/knox-copy-file.groovy
+++ mapreduce-test/src/main/resources/knox-copy-file.groovy
@@ -4,5 +4,6 @@ import org.apache.hadoop.gateway.shell.job.Job
 
 
 hadoop = Hadoop.login("https://sandbox.hortonworks.com:8443/gateway/knox_sample","guest","guest-foo")
-println Hdfs.rm(hadoop).file('/user/guest/wordcount.pig').now().string
-println Hdfs.put(hadoop).file('wordcount.pig').to('/user/guest/wordcount.pig').now().string
+Hdfs.rm(hadoop).file('/user/guest/wordcount.pig').now().string
+Hdfs.put(hadoop).file('wordcount.pig').to('/user/guest/wordcount.pig').now().string
+println "Copied file wordcount.pig to /user/guest/wordcount.pig"
